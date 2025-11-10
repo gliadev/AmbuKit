79ffcf719875ce5459468a4658bcd86522f51b70
gliadev <gliatem@gmail.com>
2025-11-10 15:12:04 +0100

    Initial Commit

---
+++ AmbuKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AmbuKit
+//
+//  Created by Adolfo on 10/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AmbuKitTests/AmbuKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmbuKitTests.swift
+//  AmbuKitTests
+//
+//  Created by Adolfo on 10/11/25.
+//
+
+import Testing
+@testable import AmbuKit
+
+struct AmbuKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
