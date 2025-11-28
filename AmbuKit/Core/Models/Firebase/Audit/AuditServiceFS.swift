caf984a0555eda587227ee2f14ca27bde9144a46
gliadev <gliatem@gmail.com>
2025-11-28 01:46:51 +0100

    fallan las vistas hay que revisar los types de vehicle

---
+++ AmbuKit/Core/Models/Firebase/Audit/AuditServiceFS.swift
@@ -0,0 +1,8 @@
+//
+//  AuditServiceFS.swift
+//  AmbuKit
+//
+//  Created by Adolfo on 27/11/25.
+//
+
+import Foundation
---
+++ AmbuKitTests/Firebase/Services/AuditServiceFSTests.swift
@@ -0,0 +1,8 @@
+//
+//  AuditServiceFSTests.swift
+//  AmbuKitTests
+//
+//  Created by Adolfo on 27/11/25.
+//
+
+import Foundation
