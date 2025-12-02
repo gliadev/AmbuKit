4eb13593ce85a6b683c40346bfb03d77b0b43481
gliadev <gliatem@gmail.com>
2025-12-03 00:22:00 +0100

    tareas 8 y 9

---
+++ AmbuKit/Core/Services/Firebase/Seed/SeedDataFirebase.swift
@@ -0,0 +1,8 @@
+//
+//  SeedDataFirebase.swift
+//  AmbuKit
+//
+//  Created by Adolfo on 1/12/25.
+//
+
+import Foundation
---
+++ AmbuKitTests/Firebase/Services/SeedDataFirebaseTests.swift
@@ -0,0 +1,8 @@
+//
+//  SeedDataFirebaseTests.swift
+//  AmbuKitTests
+//
+//  Created by Adolfo on 1/12/25.
+//
+
+import Foundation
