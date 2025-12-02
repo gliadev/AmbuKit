ff0aa21651582a32cee1ab1d3834ff3c950d49af
gliadev <gliatem@gmail.com>
2025-12-03 00:38:04 +0100

    tarea 10

---
+++ AmbuKit/Views/MainTabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabScreen.swift
+//  AmbuKit
+//
+//  Created by Adolfo on 3/12/25.
+//
+
+import SwiftUI
+
+struct MainTabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabScreen()
+}
