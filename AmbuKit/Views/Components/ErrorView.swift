c6786415b129330a05dcaf0aaa891b12fb73bade
gliadev <gliatem@gmail.com>
2025-12-09 02:05:25 +0100

    tarea 12

---
+++ AmbuKit/Views/Components/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  AmbuKit
+//
+//  Created by Adolfo on 9/12/25.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
---
+++ AmbuKit/Views/Components/KitItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  KitItemRow.swift
+//  AmbuKit
+//
+//  Created by Adolfo on 9/12/25.
+//
+
+import SwiftUI
+
+struct KitItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KitItemRow()
+}
